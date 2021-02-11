95764b1b4a9993abe04512a4fcb0e7530458cf2e
George Marios <gsqrt2@gmail.com>
2021-02-11 14:59:59 +0200

    example page and route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from './views/Home.vue';
+import Example from './views/Example.vue';
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default function createRouter() {
         path: '/',
         name: 'home.page',
         component: Home,
+      },
+      {
+        path: '/example',
+        name: 'example.page',
+        component: Example,
       },
       {
         path: '/about',
